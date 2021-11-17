922f81099745a134870d1a80e6f665e92810587a
OCaioso <94024702+OCaioso@users.noreply.github.com>
2021-11-17 15:39:28 -0300

    Botão Sair tela menu + Comando na Navigation Bar
    Adicionei função para o botão sair da tela Menu que pergunta se deseja sair da tela, se sim fecha a tela e volta para login e se não só fecha msg.
    Adicionei comando que verifica se foi pressionado o comando de voltar duas vezes para assim voltar uma tela (evita voltar caso tenha apertado sem querer)

--- app/src/main/java/com/example/rrsolucoeshotel/activities/FormLogin.java
+++ app/src/main/java/com/example/rrsolucoeshotel/activities/FormLogin.java
@@ -1,14 +1,15 @@
 package com.example.rrsolucoeshotel.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.example.rrsolucoeshotel.R;
@@ -23,8 +24,9 @@ public class FormLogin extends AppCompatActivity {
     private Button btnAcessar;
     private ProgressBar progressBar;
 
-    private static final String[] MENSAGENS_ERRO = {"Preencha todos os campos.",
-            "Email não cadastrado.", "Senha incorreta."};
+    boolean botaoVoltarClicadoDuasVezes;
+    private static final String[] MENSAGENS = {"Preencha todos os campos.",
+            "Email não cadastrado.", "Senha incorreta.", "Toque novamente para sair"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +37,7 @@ public class FormLogin extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         IniciarComponentes();
-        AcessarApp();
+        ClicarAcessar();
     }
 
     @Override
@@ -47,12 +49,34 @@ public class FormLogin extends AppCompatActivity {
         progressBar.setVisibility(View.INVISIBLE);
     }
 
-    private void AcessarApp() {
+    @Override
+    public void onBackPressed() {
+        if (botaoVoltarClicadoDuasVezes) {
+            super.onBackPressed();
+            return;
+        }
+
+        ConfiguraClicarDuasVezes();
+        VerificaCliqueDuplo();
+    }
+
+    private void VerificaCliqueDuplo() {
+        new Handler().postDelayed(() -> botaoVoltarClicadoDuasVezes = false, 2000);
+    }
+
+    private void ConfiguraClicarDuasVezes() {
+        ConstraintLayout layout = findViewById(R.id.ConstraintLayout_login);
+
+        this.botaoVoltarClicadoDuasVezes = true;
+        SnackbarMsgs(layout, 3);
+    }
+
+    private void ClicarAcessar() {
         btnAcessar.setOnClickListener(view -> {
+            progressBar.setVisibility(View.VISIBLE);
+
             String email = edEmail.getText().toString();
             String senha = edSenha.getText().toString();
-
-            progressBar.setVisibility(View.VISIBLE);
 
             if (email.isEmpty() || senha.isEmpty()) {
                 //Msg não preenchido
@@ -85,12 +109,12 @@ public class FormLogin extends AppCompatActivity {
         Intent logar = new Intent(getApplicationContext(),
                 FormMenuServicos.class);
         logar.putExtra("emailUsado", email); //passando os dados do email para próxima atcivity
-
+        //finish();
         startActivity(logar);
     }
 
     private void SnackbarMsgs(View view, int i) {
-        Snackbar barraMsg = Snackbar.make(view, MENSAGENS_ERRO[i], Snackbar.LENGTH_SHORT);
+        Snackbar barraMsg = Snackbar.make(view, MENSAGENS[i], Snackbar.LENGTH_SHORT);
         barraMsg.setBackgroundTint(Color.WHITE);
         barraMsg.setTextColor(Color.BLACK);
         barraMsg.show();
@@ -101,6 +125,7 @@ public class FormLogin extends AppCompatActivity {
         edSenha = findViewById(R.id.txtSenhaLogin);
         btnAcessar = findViewById(R.id.btnAcessarLogin);
         progressBar = findViewById(R.id.progressBarLogin);
+        botaoVoltarClicadoDuasVezes = false;
     }
 
 }
--- app/src/main/java/com/example/rrsolucoeshotel/activities/FormMenuServicos.java
+++ app/src/main/java/com/example/rrsolucoeshotel/activities/FormMenuServicos.java
@@ -1,19 +1,34 @@
 package com.example.rrsolucoeshotel.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rrsolucoeshotel.R;
 import com.example.rrsolucoeshotel.model.BDHelper;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
 
 public class FormMenuServicos extends AppCompatActivity {
 
-    TextView edBVindo;
-    String emailHospede;
+    private TextView edBVindo;
+    private Button btSair;
+
+    boolean botaoVoltarClicadoDuasVezes;
+    private String emailHospede;
+    private static final String TXT_BVINDO = "Bem vindo, ";
+    private static final String MSG_VOLTAR = "Toque novamente para voltar";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +40,75 @@ public class FormMenuServicos extends AppCompatActivity {
 
         IniciarComponentes();
         adicionarNomeHospede();
+
+        ClicarSairFormMenuServicos();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (botaoVoltarClicadoDuasVezes) {
+            super.onBackPressed();
+            return;
+        }
+
+        ConfiguraClicarDuasVezes();
+        VerificaCliqueDuplo();
+    }
+
+    private void VerificaCliqueDuplo() {
+        new Handler().postDelayed(() -> botaoVoltarClicadoDuasVezes = false, 2000);
+    }
+
+    private void ConfiguraClicarDuasVezes() {
+        ConstraintLayout layout = findViewById(R.id.ConstraintLayout_login);
+
+        this.botaoVoltarClicadoDuasVezes = true;
+        SnackbarMsg(layout);
+    }
+
+    private void ClicarSairFormMenuServicos() {
+        btSair.setOnClickListener(view -> {
+            CriaCaixaDialogo();
+        });
+    }
+
+    private void CriaCaixaDialogo() {
+        AlertDialog.Builder caixaDialogo = new AlertDialog.Builder(this);
+        caixaDialogo.setTitle("Alerta!");
+        caixaDialogo.setIcon(R.drawable.ic_feedback);
+        caixaDialogo.setMessage("Tem certeza que deseja voltar para a tela de Login?");
+        caixaDialogo.setPositiveButton("Sim", (dialogInterface, i) -> {
+            Log.w("caixaDialogo", "Clicou sim");
+            Deslogar();
+        });
+        caixaDialogo.setNegativeButton("Não", (dialogInterface, i) ->
+                Log.w("caixaDialogo", "Clicou não"));
+        caixaDialogo.show();
+    }
+
+    private void Deslogar() {
+        Intent voltarLogin = new Intent(getApplicationContext(), FormLogin.class);
+        finish();
+        startActivity(voltarLogin);
     }
 
     private void IniciarComponentes() {
         edBVindo = findViewById(R.id.txtBemVindo);
+        btSair = findViewById(R.id.btnSairMenu);
         emailHospede = getIntent().getStringExtra("emailUsado");
+        botaoVoltarClicadoDuasVezes = false;
     }
 
     private void adicionarNomeHospede() {
         BDHelper bancoDados = new BDHelper();
 
-        edBVindo.setText("Bem vindo, " + bancoDados.RetornarNomeCliente(emailHospede));
+        edBVindo.setText(TXT_BVINDO + bancoDados.RetornarNomeCliente(emailHospede));
+    }
+
+    private void SnackbarMsg(View view) {
+        Snackbar barraMsg = Snackbar.make(view, MSG_VOLTAR, Snackbar.LENGTH_SHORT);
+        barraMsg.setBackgroundTint(Color.WHITE);
+        barraMsg.setTextColor(Color.BLACK);
+        barraMsg.show();
     }
 }
